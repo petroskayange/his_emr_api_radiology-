02a1006c072a25f33307c62321410ada1cead789
petros <kayangepetros@gmail.com>
2021-05-09 15:00:55 -0400

    first commit

--- app/controllers/radiology/radiology_controller.rb
+++ app/controllers/radiology/radiology_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
-class Radiology::RadiologyController < ::ApplicationController
+require 'zebra_printer/init'
+class Radiology::RadiologyOrdersController < ::ApplicationController
+  before_action :authenticate, except: %i[print_order_label ]
   def create
     patient_details, physician_details, radiology_orders = params.require %i[patient_details physician_details radiology_orders]
     radiology_orders = service.generate_msi(patient_details,physician_details, radiology_orders)
     render json: radiology_orders, status: :created
   end
-
+  def print_order_label
+    label = service.print_radiology_barcode(params[:accession_number],params[:patient_national_id], params[:patient_name], params[:radio_order], params[:date_created])
+    send_data(label, type: 'application/label; charset=utf-8',
+                           stream: false,
+                           filename: "#{SecureRandom.hex(24)}.lbl",
+                           disposition: 'inline')
+  end
   def service
     Radiology::RadiologyService
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Radiology::Engine.routes.draw do
-    resources :radiology, path: 'api/v1/radiology'
+    resources :radiology, path: 'api/v1/radiology/radiology_orders'
     resources :radiology_properties, path: 'api/v1/radiology_properties'
       
-    get '/api/v1/radiology/barcode', to: 'labels#print_order_label'
+    get '/api/v1/radiology/barcode', to: 'radiology#print_order_label'
     
 end
